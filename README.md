6f88b7e37659f0180d867c1b68d0b971f75dac93
mrchuvyzgalov <mr.chuvyzgalov@gmail.com>
2024-05-20 19:17:49 +0200

    Added readme.md

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# integrals
-# integrals
+# Functional programming final project
+## Task: Computational mathematics
+You can read about different computational methods for integrals [here](https://habr.com/ru/articles/420867/)
+
+Minimum:
+* (10 points) A library
+* (10 points) The user choose the error and a definite integral as a Haskell function
+* (30 points) The implementation of 3 different strategies
+* (5 points) Error reporting
+* (5 points) Unit tests
+
+Additional tasks:
+* (10 points) Console interface allowing the user to specify the integral without writing any Haskell code
+* (10 points) Computation of the error and how many steps it took to get to the final result
+* (10 points) Comparison of the methods
+* (10 points) Property-based testing
+
+## The architecture of the solution
+### Data 
+* **Error** - responsible for error types: **NsegIsNotPositive** (amount of segments <= 0), **ErrorIsNotPositive** (erro <= 0)
+* **RectangleRule** - responsible for rectangle strategy type: **LeftPoint** (left rectangle rule), **RightPoint** (right rectangle rule), **MidPoint** (midpoint rectangle rule)
+* **Strategy** - responsible for strategy type: **Rectangle** (rectangle strategy), **Trapezoid** (trapezoid strategy), **Simpson** (siimpson startegy)
+* **Params** - responsible for computation integral params
+
+## Computation intergal functions
+* **compute'integral'by'rectangle'strategy** - responsible for computation integral by rectangle strategy
+* **compute'integral'by'trapezoid'strategy'without'estimation** - responsible for computation integral by trapezoid strategy without estimation
+* **compute'integral'by'trapezoid'strategy** - responsible for computation integral by trapezoid strategy
+* **compute'integral'by'simpson'strategy** - responsible for computation integral by simpson strategy
+* **compute'integral** - responsible for computation integral
+
+The main function is **compute'integral**. You have to provide parameter (**Params**) and function (**Double -> Double**). The function compute integral by according strategy and then returns the answer (**Either Error Double**). If the result is computed, it is **Right Double**, otherwise it is **Left Error**
--- src/Integral.hs
+++ src/Integral.hs
@@ -13,62 +13,62 @@ rectangleRuleFrac = [
     (MidPoint, 0.5)]
 
 compute'integral'by'rectangle'strategy :: Params -> (Double -> Double) -> Either Error Double
-compute'integral'by'rectangle'strategy Params { strategy = Rectangle { rule = rule' }, lowerLimit = ll , upperLimit = ul , nseg = nseg' } f 
+compute'integral'by'rectangle'strategy Params { strategy = Rectangle { rule = rule' }, lowerLimit = ll , upperLimit = ul , nseg = nseg' } f
     | nseg' <= 0 = Left NsegIsNotPositive { ns = nseg' }
-    | otherwise = let 
-        dx = (ul - ll) / (int2Double nseg')
+    | otherwise = let
+        dx = (ul - ll) / int2Double nseg'
         frac = fromJust $ lookup rule' rectangleRuleFrac
         xstart = ll + frac * dx
-        in Right $ (* dx) $ sum $ map f $ take nseg' $ [xstart, xstart + dx ..]  
+        in Right $ (* dx) $ sum $ map f $ take nseg' [xstart, xstart + dx ..]
 
 compute'integral'by'trapezoid'strategy'without'estimation :: Params -> (Double -> Double ) ->  Double
 compute'integral'by'trapezoid'strategy'without'estimation Params { lowerLimit = ll , upperLimit = ul , nseg = nseg' } f = let
-        dx = (ul - ll) / (int2Double nseg')
-        xstart = ll + dx 
+        dx = (ul - ll) / int2Double nseg'
+        xstart = ll + dx
         sum0 = 0.5 * (f ll + f ul)
-        in (* dx) $ (+ sum0) $ sum $ map f $ take (nseg' - 1) $ [xstart, xstart + dx..]
+        in (* dx) $ (+ sum0) $ sum $ map f $ take (nseg' - 1) [xstart, xstart + dx..]
 
 compute'integral'by'trapezoid'strategy :: Params -> (Double -> Double) -> Either Error Double
-compute'integral'by'trapezoid'strategy ps@(Params { strategy = Trapezoid { estError = error' }, nseg = nseg' }) f 
+compute'integral'by'trapezoid'strategy ps@(Params { strategy = Trapezoid { estError = error' }, nseg = nseg' }) f
     | nseg' <= 0 = Left NsegIsNotPositive { ns = nseg' }
     | error' <= 0 = Left ErrorIsNotPositive { err = error' }
     | otherwise = let
         ans = compute'integral'by'trapezoid'strategy'without'estimation ps f
         errEst = max 1 $ abs ans
         in Right $ estimate errEst error' ps f ans
-        where 
+        where
             estimate currErr err ps@(Params { nseg = nseg' } ) f ans
                 | currErr <= abs (err * ans) = ans
-                | otherwise = let 
+                | otherwise = let
                     newPs = ps { nseg = 2 * nseg'  }
                     newAns = compute'integral'by'trapezoid'strategy'without'estimation newPs f
-                    newCurrErr = abs $ ans - newAns 
+                    newCurrErr = abs $ ans - newAns
                     in estimate newCurrErr err newPs f newAns
 
 compute'integral'by'simpson'strategy :: Params -> (Double -> Double) -> Either Error Double
-compute'integral'by'simpson'strategy ps@(Params { strategy = Simpson { estError = error' }, nseg = nseg' }) f 
+compute'integral'by'simpson'strategy ps@(Params { strategy = Simpson { estError = error' }, nseg = nseg' }) f
     | nseg' <= 0 = Left NsegIsNotPositive { ns = nseg' }
     | error' <= 0 = Left ErrorIsNotPositive { err = error' }
-    | otherwise = let 
-        oldTrapezSum = compute'integral'by'trapezoid'strategy'without'estimation ps f 
+    | otherwise = let
+        oldTrapezSum = compute'integral'by'trapezoid'strategy'without'estimation ps f
         newPs = ps { nseg = 2 * nseg' }
-        newTrapezSum = compute'integral'by'trapezoid'strategy'without'estimation newPs f 
+        newTrapezSum = compute'integral'by'trapezoid'strategy'without'estimation newPs f
         ans = computeAns oldTrapezSum newTrapezSum
         errEst = max 1 $ abs ans
         in Right $ estimate errEst error' newPs newTrapezSum f ans
-        where 
+        where
             estimate currErr err ps@(Params { nseg = nseg' } ) oldTrapezSum f ans
                 | currErr <= abs (err * ans) = ans
-                | otherwise = let 
+                | otherwise = let
                     newPs = ps { nseg = 2 * nseg' }
-                    newTrapezSum = compute'integral'by'trapezoid'strategy'without'estimation newPs f 
+                    newTrapezSum = compute'integral'by'trapezoid'strategy'without'estimation newPs f
                     newAns = computeAns oldTrapezSum newTrapezSum
-                    newCurrErr = abs $ ans - newAns 
+                    newCurrErr = abs $ ans - newAns
                     in estimate newCurrErr err newPs newTrapezSum f newAns
 
             computeAns oldTrapezSum newTrapezSum = (4.0 * newTrapezSum - oldTrapezSum) / 3.0
 
 compute'integral :: Params -> (Double -> Double) -> Either Error Double
-compute'integral ps@(Params { strategy = Rectangle {}} ) f = compute'integral'by'rectangle'strategy ps f 
-compute'integral ps@(Params { strategy = Trapezoid {}} ) f = compute'integral'by'trapezoid'strategy ps f 
-compute'integral ps@(Params { strategy = Simpson {}} ) f = compute'integral'by'simpson'strategy ps f 
+compute'integral ps@(Params { strategy = Rectangle {}} ) f = compute'integral'by'rectangle'strategy ps f
+compute'integral ps@(Params { strategy = Trapezoid {}} ) f = compute'integral'by'trapezoid'strategy ps f
+compute'integral ps@(Params { strategy = Simpson {}} ) f = compute'integral'by'simpson'strategy ps f
